6f1448c8ed963b118af863711d230bad6fbfdde5
jbnimble <jbnimbledev@gmail.com>
2024-12-07 21:57:23 -0500

    Add day04 part2 solution

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -15,16 +15,26 @@ fn main() {
     let indexes_vertical = transpose(vertical_starters, vertical_calculate, width, length);
     let contents_vertical = move_indexes(graphemes.clone(), indexes_vertical);
     let indexes_left_diagonal = transpose(diagonal_left_starters, diagonal_left_calculate, width, length);
-    let contents_left_diagonal = move_indexes(graphemes.clone(), indexes_left_diagonal);
+    let contents_left_diagonal = move_indexes(graphemes.clone(), indexes_left_diagonal.clone());
     let indexes_right_diagonal = transpose(diagonal_right_starters, diagonal_right_calculate, width, length);
-    let contents_right_diagonal = move_indexes(graphemes.clone(), indexes_right_diagonal);
+    let contents_right_diagonal = move_indexes(graphemes.clone(), indexes_right_diagonal.clone());
     let horizontal_count = xmas_counter(contents);
     let vertical_count = xmas_counter(contents_vertical);
-    let diagonal_left_count = xmas_counter(contents_left_diagonal);
-    let diagonal_right_count = xmas_counter(contents_right_diagonal);
+    let diagonal_left_count = xmas_counter(contents_left_diagonal.clone());
+    let diagonal_right_count = xmas_counter(contents_right_diagonal.clone());
     let result_part1 = vertical_count + horizontal_count + diagonal_left_count + diagonal_right_count;
 
+    // result of searching for "XMAS" left, right, up, down, diagonal left, diagonal right and overlapping
     println!("Part1 = {result_part1} h {horizontal_count} v {vertical_count} d {diagonal_left_count} {diagonal_right_count}");
+
+    let mut left_x_result = mas_x_finder(contents_left_diagonal.clone(), indexes_left_diagonal.clone());
+    let mut right_x_result = mas_x_finder(contents_right_diagonal.clone(), indexes_right_diagonal.clone());
+    left_x_result.sort();
+    right_x_result.sort();
+    let result_part2 = left_x_result.iter().filter(|&p| right_x_result.contains(p)).count();
+
+    // result of "MAS" in 2 diagonal directions sharing the "A", so it forms an X pattern 
+    println!("Part2 = {}", result_part2)
 }
 
 /* find_iter results are non-overlapping, must search forwards and backwards separately to handle XMASAMX and SAMXMAS cases */
@@ -86,4 +96,21 @@ fn move_indexes(contents: Vec<&str>, indexes: Vec<Vec<usize>>) -> String {
         result.push("\n");
     }
     result.into_iter().collect()
+}
+
+// find pre-diagonalized index position of "A", add to a left and right lists, compare
+fn mas_x_finder(contents: String, indexes: Vec<Vec<usize>>) -> Vec<usize> {
+    let mut result_index: usize = 0;
+    let mut result:Vec<usize> = Vec::new();
+    for line in contents.lines() {
+        let matches: Vec<_> = line.match_indices("MAS").chain(line.match_indices("SAM")).collect();
+        if !matches.is_empty() {
+            for item in matches {
+                result.push(indexes.clone()[result_index][item.0 + 1]);
+
+            }
+        }
+        result_index = result_index + 1;
+    }
+    result
 }
